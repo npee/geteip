d61f9dee66415301d5163bdf55460c9219231efd
MyeongchanJin <jmcchan@naver.com>
2020-07-12 23:53:52 +0900

    ItemService 인터페이스 파라미터 타입 수정

--- src/main/java/com/npee/eip/service/ItemService.java
+++ src/main/java/com/npee/eip/service/ItemService.java
@@ -2,14 +2,13 @@ package com.npee.eip.service;
 
 import com.npee.eip.model.entity.Item;
 import com.npee.eip.model.request.RequestItemDto;
-import com.npee.eip.model.request.RequestQuizDto;
 
 import java.util.List;
 
 public interface ItemService {
-    Item insertItemSet(RequestItemDto quizDto);
+    Item insertItemSet(RequestItemDto itemDto);
     List<Item> selectItemSets();
     Item selectAItemSet(Long itemId);
-    Item updateItemSet(Long itemId, RequestQuizDto quizDto);
+    Item updateItemSet(Long itemId, RequestItemDto itemDto);
     void deleteItemSet(Long itemId);
 }

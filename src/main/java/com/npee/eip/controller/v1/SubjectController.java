585cf0bb6950502b21f343bca91dc8679b8c1672
MyeongchanJin <jmcchan@naver.com>
2020-07-12 23:53:12 +0900

    @ApiImplicitParams 테스트

--- src/main/java/com/npee/eip/controller/v1/SubjectController.java
+++ src/main/java/com/npee/eip/controller/v1/SubjectController.java
@@ -5,8 +5,7 @@ import com.npee.eip.config.response.ResponseService;
 import com.npee.eip.config.response.SingleResult;
 import com.npee.eip.model.entity.Subject;
 import com.npee.eip.service.SubjectServiceImpl;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -29,6 +28,9 @@ public class SubjectController {
     }
 
     @ApiOperation(value = "과목 단일 출력", notes = "과목 하나를 조회한다.")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "subjectId", value = "과목 번호", required = true, dataType = "long", paramType = "path"),
+    })
     @GetMapping("/{subjectId}")
     public SingleResult<Subject> getSelectedSubject(@PathVariable Long subjectId) {
         return responseService.getSingleResult(subjectService.selectASubject(subjectId));

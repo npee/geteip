f4a223f1025ed15c22183fa6866172b7e6d4b81a
MyeongchanJin <jmcchan@naver.com>
2020-07-14 11:20:44 +0900

    NullPointerException 방지 위한 List 초기화

--- src/main/java/com/npee/eip/model/request/RequestQuizDto.java
+++ src/main/java/com/npee/eip/model/request/RequestQuizDto.java
@@ -1,10 +1,14 @@
 package com.npee.eip.model.request;
 
 import com.npee.eip.model.entity.Comment;
+import com.npee.eip.model.entity.Item;
 import com.npee.eip.model.entity.Quiz;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -17,18 +21,8 @@ public class RequestQuizDto {
     private String isCorrect;
     // 과목 번호
     private Long subjectId;
-    // 보기 리스트 TODO: ItemDto로 이관
-    // private List<Item> itemList;
+    // 보기 리스트
+    private List<Item> items = new ArrayList<>();
     // 해설 번호
     private Comment tempComment;
-
-    public Quiz toEntity() {
-        return Quiz.builder()
-                .year(year)
-                .nth(nth)
-                .question(question)
-                .image(image)
-                .isCorrect(isCorrect)
-                .build();
-    }
 }

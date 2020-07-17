dc156df2488cab12f11d74355cc55867a5806ea7
MyeongchanJin <jmcchan@naver.com>
2020-07-17 18:43:47 +0900

    ResponseDto에 매핑되도록 쿼리 직접 작성하여 인터페이스에 등록

--- src/main/java/com/npee/eip/repository/QuizJpaRepository.java
+++ src/main/java/com/npee/eip/repository/QuizJpaRepository.java
@@ -1,8 +1,16 @@
 package com.npee.eip.repository;
 
 import com.npee.eip.model.entity.Quiz;
+import com.npee.eip.model.response.ResponseQuizCountDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface QuizJpaRepository extends JpaRepository<Quiz, Long> {
     Long countByYearAndNthAndSubjectId(Long Year, String nth, Long subjectId);
+    @Query("SELECT new com.npee.eip.model.response.ResponseQuizCountDto(q1.year, q1.nth, q1.subjectId, COUNT(DISTINCT q2.quizId))" +
+            "FROM Quiz q1 LEFT JOIN Quiz q2 ON q1.year = q2.year AND q1.nth = q2.nth AND q1.subjectId = q2.subjectId GROUP BY " +
+            "q1.year, q1.nth, q1.subjectId")
+    List<ResponseQuizCountDto> selectQuizCountByYearAndNthAndSubjectId();
 }
